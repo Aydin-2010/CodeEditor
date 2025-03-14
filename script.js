2d1158c4c48d912ae21a4538d3acbd56a0e8eecc
Aydin-2010 <aydinabdullahaziz@gmail.com>
2025-03-14 16:27:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ var editor = CodeMirror.fromTextArea(document.getElementById("InputField"), {
     lineWrapping: true,  // Wrap lines instead of horizontal scroll
     matchBrackets: true, // Highlight matching brackets
     autoCloseBrackets: true, // Auto-close brackets
-    foldGutter: true,    // Enable code folding
-    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+    /*foldGutter: true,    // Enable code folding
+    gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]*/
 });
