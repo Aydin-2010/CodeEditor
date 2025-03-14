76d917a26851a03c547a7cea7c9128ceb10fef27
Aydin-2010 <aydinabdullahaziz@gmail.com>
2025-03-14 19:27:12 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const editor = CodeMirror.fromTextArea(document.getElementById("InputField"), {
   mode: "javascript",
+  theme: "dracula",
   lineWrapping: true,
-  indentUnit: 4,
-  smartIndent: true,
   lineNumbers: true,
   matchBrackets: true,
   autoCloseBrackets: true,
+  foldGutter: false,
 });
