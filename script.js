0e3829f6713a16a3b1272a5d49eb32d47c030bf3
Aydin-2010 <aydinabdullahaziz@gmail.com>
2025-03-14 19:10:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+const editor = CodeMirror.fromTextArea(document.getElementById("InputField")), {
+  lineWrapping: true,
+  indentUnit: 4,
+  smartIndent: true,
+  lineNumbers: true,
+  matchBrackets: true,
+  autoCloseBrackets: true,
+}
